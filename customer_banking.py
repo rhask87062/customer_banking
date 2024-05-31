9dc6daa5ae6ebc530502ee60c397729f1c2fe8b2
rhask87062 <rhask87062@gmail.com>
2024-05-31 13:35:53 -0500

    Final push of assignment files.

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -9,21 +11,30 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input("Please enter youre savings account balance: "))
+    savings_interest = float(input("Please enter your interest rate: "))
+    savings_maturity = int(input("Please enter the number of months to calculate: "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(interest_earned, updated_savings_balance, savings_maturity)
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input("Please enter your cd balance: "))
+    cd_interest = float(input("Please input your interest rate: "))
+    cd_maturity = int(input("Please enter the number of months to calculate: "))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(interest_earned, updated_cd_balance, cd_maturity)
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
